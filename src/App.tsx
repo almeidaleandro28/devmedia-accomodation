2e001832f977f3e080b62a29fa24f81ddf4b3c81
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-08-17 07:54:26 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center text-red-700 uppercase'>title app</h1>
+      <p className="text-center text-green-500 uppercase ">more text</p>
+        
+    </>
+  )
+}
+
+export default App
